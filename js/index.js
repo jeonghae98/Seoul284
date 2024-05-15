d3f0c4e8fc09459efde037108a0bc456d0a0c073
jeonghae98 <answjdgo8@gmail.com>
2024-05-16 08:50:06 +0900

    0516 / 0849L

--- js/index.js
+++ js/index.js
@@ -2,4 +2,8 @@ $(function() {
     $('#mobile-menu').click(function() {
         $(this).toggleClass('active');
     });
+
+    $(".circle-text").html(function() {
+        return $(this).text().split("").map((char, i) => `<span style="transform:rotate(${i * 15}deg)">${char}</span>`).join("");
+    });
 });
