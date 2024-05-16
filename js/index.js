e8b92f5d2f53121609a1ff56938b7bb51bff6744
jeonghae98 <answjdgo8@gmail.com>
2024-05-16 16:40:12 +0900

    0516 / 1640L

--- js/index.js
+++ js/index.js
@@ -3,6 +3,22 @@ $(function() {
         $(this).toggleClass('active');
     });
 
+    $(window).scroll(function() {
+        var scrollPosition = $(this).scrollTop();
+        var elementOffset = $('.main-intro').offset().top;
+        var windowHeight = $(window).height();
+        
+        // console.log(elementOffset);
+        // console.log(windowHeight);
+
+        if (scrollPosition > (elementOffset - windowHeight)) {
+            $('.main-intro .line').animate({
+                height: '1072px'
+              }, 3000);
+        }
+    });
+
+
     $(".circle-text").html(function() {
         return $(this).text().split("").map((char, i) => `<span style="transform:rotate(${i * 15}deg)">${char}</span>`).join("");
     });
