9deb8a1b3d6b1b32eecceec5e3cc0b3c4cde634d
jeonghae98 <answjdgo8@gmail.com>
2024-05-24 00:51:56 +0900

    0524 /// 1251L

--- 3. referenceroom/js/referenceroom.js
+++ 3. referenceroom/js/referenceroom.js
@@ -7,9 +7,9 @@ $(function() {
 
 
     //===================== main =====================
-    $('.list:eq(0)').on('click', function() {
-        window.location.href = './recruitment/detail-page_05.html';
-    });
+    // $('.list:eq(0)').on('click', function() {
+    //     window.location.href = './recruitment/detail-page_05.html';
+    // });
 
     
     //===================== footer =====================
