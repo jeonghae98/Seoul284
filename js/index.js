25537a6269dd765996607f35d84b44b2dc22852e
jeonghae98 <answjdgo8@gmail.com>
2024-05-31 01:01:54 +0900

    [05/31] 1차 header 수정

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 $(function() {
     //===================== header =====================
+    //--------------------- 모바일 ---------------------
     var mobile = $('#mobileBar');
     
     mobile.on('click', function() {
@@ -9,6 +10,10 @@ $(function() {
         mobile.toggleClass('active');  
         
         
+        // <search 없애기>
+        $('.search').toggleClass('hide');
+
+
 
         // <모바일 메뉴>
         mainMenu.toggleClass('active');
@@ -20,7 +25,7 @@ $(function() {
                 'position': 'fixed',
                 'top': '12vh',
                 'right': 0,
-                'border-top': '1px solid #dbdbdb',
+                'borderTop': '1px solid #dbdbdb',
                 'width': '100%',
                 'height': '100%',
                 'background-color': '#fff'
@@ -31,28 +36,80 @@ $(function() {
         }
         
         // #sub-menu 나오게 하기
-        $('.gnb-title').click(function(e) {
+        $('.gnb-title').on('click', function(e) {
             e.preventDefault();
         
-            // 클릭된 메뉴의 부모 li 요소 찾기
-            var $parentLi = $(this).parent('li');
+            var $parentLi = $(this).parent('li');  // 클릭된 메뉴의 부모 li 요소 찾기
         
-            // 클릭된 메뉴의 하위 메뉴를 슬라이드 토글
-            $parentLi.find('.sub-menu').stop(true, true).slideToggle(1000);
+            $parentLi.find('.sub-menu').stop().slideToggle(500);  // 클릭된 메뉴의 하위 메뉴를 슬라이드 토글
         
-            // 다른 모든 하위 메뉴를 슬라이드 업
-            $('.gnb li').not($parentLi).find('.sub-menu').stop(true, true).slideUp(1000);
+            $('.gnb li').not($parentLi).find('.sub-menu').stop().slideUp(500);  // 다른 모든 하위 메뉴를 슬라이드 업
+        
+        
+            // #gnb-title 눌렀을 때 모습
+            $('.gnb-title').removeClass('highlight');  // 모든 .gnb-title 요소에서 highlight 클래스 제거
+    
+            if (!$(this).hasClass('highlight')) {
+                $(this).addClass('highlight');
+            }  // 클릭한 요소의 현재 상태에 따라 highlight 클래스 추가 또는 제거
+           
         });
         
+
+        // #sub-menu 눌렀을 때 모습
+        $('.sub-menu a').click(function() {
+            $('.sub-menu a').css({  
+                'color': '',
+                'textDecoration': ''
+            });
+    
+    
+            $(this).css({  
+                'color': '#000',
+                'textDecoration': 'underline'
+            });
+        });
     });
 
 
 
-    //===================== main =====================
-        //--------------------- main 공통 ---------------------
-        $(".circle-text").html(function() {
-            return $(this).text().split("").map((char, i) => `<span style="transform:rotate(${i * 15}deg)">${char}</span>`).join("");
+    //--------------------- PC ---------------------
+    // $('.gnb-title').hover(function() {
+    //     // 마우스가 올라갔을 때 서브메뉴를 내림
+    //     $(this).next('.sub-menu').stop().slideDown(500);
+    // }, function() {
+    //     // 마우스가 벗어났을 때 서브메뉴를 올림
+    //     $(this).next('.sub-menu').stop().slideUp(500);
+    // });
+    var pcWindowWidth = $(window).width() > 768;
+    var $subMenu = $('.gnb-title').next('.sub-menu');
+
+    if (pcWindowWidth) {
+        $('.gnb-title').hover(function() {
+            // 마우스가 올라갔을 때 서브메뉴를 내림
+            $subMenu.stop().slideDown(500);
+            $subMenu.addClass('active');
+            $('.sub-menu > li > a').css('padding', '10px 0');
         });
+
+        $('.gnb').hover(function() {
+            // gnb 내부에서는 아무것도 하지 않음
+        }, function() {
+            // 마우스가 gnb 영역을 벗어났을 때 모든 서브메뉴를 올림
+            $('.sub-menu').stop().slideUp(500);
+        });
+    }
+
+
+
+
+
+
+    //===================== main =====================
+    //--------------------- main 공통 ---------------------
+    $(".circle-text").html(function() {
+        return $(this).text().split("").map((char, i) => `<span style="transform:rotate(${i * 15}deg)">${char}</span>`).join("");
+    });
 
 
     //--------------------- intro ---------------------
@@ -367,7 +424,7 @@ $(function() {
             var imgOffset = $img.offset().top;
     
             if (windowMiddle > imgOffset && $img.css('opacity') == 0) {
-                if (mainWindowWidth < 786) {
+                if (mainWindowWidth < 768) {
                     $img.animate({
                         opacity: 1
                     }, 1300);
