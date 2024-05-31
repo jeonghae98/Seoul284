565f3058467ff136b61140adb2cb0eed69be2833
jeonghae98 <answjdgo8@gmail.com>
2024-05-31 21:09:16 +0900

    [05/31] 2108L

--- 4.inform/js/notice_2.js
+++ 4.inform/js/notice_2.js
@@ -7,6 +7,31 @@ $(function() {
 
 
     //===================== main =====================
+    // <공통 애니메이션>
+    $('.inform-title').css('opacity', '1');
+    $('.menu-list').css('opacity', '1');
+
+    function Visibility() {  // 리스트 애니메이션
+        $('.list').each(function() {
+            var listTop = $(this).offset().top;
+            var listBottom = listTop + $(this).outerHeight();
+            var viewportTop = $(window).scrollTop();
+            var viewportBottom = viewportTop + $(window).height();
+
+            if (listBottom > viewportTop && listTop < viewportBottom) {
+                $(this).addClass('visible');
+            } else {
+                $(this).removeClass('visible');
+            }
+        });
+    }
+
+    $(window).on('scroll resize', Visibility);
+    $(window).trigger('scroll');
+    
+    
+    
+    
     let i = 0;
 
     $('.ntc-list').each(function(i) {
