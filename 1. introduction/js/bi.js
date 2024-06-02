c3f41ec0d06025bd5f0d5caee126d2ada99faa98
Leesungmin98 <cjrdl22@naver.com>
2024-06-03 06:44:07 +0900

    0603/06:43

--- 1. introduction/js/bi.js
+++ 1. introduction/js/bi.js
@@ -1,11 +1,44 @@
 $(function() {
     $(window).scroll(function() {
-        let $scrollLeft = $(this).scrollLeft(); // 수평 스크롤바의 위치인식
-
-
+         
+      
         let $scrollTop = $(this).scrollTop(); // 수직 스크롤바의 위치인식
         console.log($scrollTop);
 
+        if($(window).width() <= 768) {
+            // mobile only code
+            if ($scrollTop < 600) {
+                $('.sec2-title h2').stop().animate({ opacity: '0' }, 300);
+            } else {
+                $('.sec2-title h2').stop().animate({ opacity: '1' }, 300);
+            }
+
+            if ($scrollTop < 600) {
+                $('.sec2-title p').stop().animate({ opacity: '0' }, 300);
+            } else {
+                $('.sec2-title p').stop().animate({ opacity: '1' }, 300);
+            }
+                
+            if ($scrollTop < 700) {
+                $('.img-box').stop().animate({ marginTop: '1000px' }, 1000);
+            } else {
+                $('.img-box').stop().animate({ marginTop: '0px' }, 1000);
+            }
+
+            if ($scrollTop < 1300) {
+                $('.sec3').stop().animate({ opacity: '0' }, 1000);
+            } else {
+                $('.sec3').stop().animate({ opacity: '1' }, 1000);
+            }
+            
+    
+            if ($scrollTop < 2300) {
+                $('.color').stop().animate({ opacity: '0', height: '0px' }, 1000);
+            } else {
+                $('.color').stop().animate({ opacity: '1' ,height: '1300px' }, 1000);
+            }
+        } else {
+            
         if ($scrollTop < 600) {
             $('.sec2-title h2').stop().animate({ opacity: '0' }, 300);
         } else {
@@ -35,6 +68,7 @@ $(function() {
             $('.color').stop().animate({ opacity: '0', height: '0px' }, 1000);
         } else {
             $('.color').stop().animate({ opacity: '1' ,height: '600px' }, 1000);
+        }
         }
 
 
